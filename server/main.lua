f5c25cb2583bd4a6bd6e64b0abe9e6b104da64c7
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-11-06 12:57:55 -0500

    linting + optimization

--- server/main.lua
+++ server/main.lua
@@ -14,80 +14,82 @@ end
 
 RegisterNetEvent('brazzers-market:server:setOwner', function(market, password)
     local src = source
-    if not src then return end
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
+    if not market or not password then return end
 
-    local citizenId = Player.PlayerData.citizenid
-    local claimBooth = exports['5life-businesses']:canClaimBooth(citizenId)
-    if not claimBooth then return TriggerClientEvent('DoLongHudText', src, 'You cannot claim this booth', 2) end
+    local CID = Player.PlayerData.citizenid
 
-    if Config.Market[market]['owner'] then return TriggerClientEvent("DoLongHudText", src, 'This booth has already been claimed!', 2) end
+    if Config.Market[market]['owner'] then return TriggerClientEvent('QBCore:Notify', src, Lang:t("error.already_claimed"), 'error') end
 
-    for _, v in pairs(Config.Market) do
-        if v['owner'] == citizenId then
-            TriggerClientEvent("DoLongHudText", src, 'You already have a claimed booth!', 2)
-            return
+    if not Config.AllowMultipleClaims then
+        for _, v in pairs(Config.Market) do
+            if v['owner'] == CID then
+                TriggerClientEvent('QBCore:Notify', src, Lang:t("error.existing_booth"), 'error')
+                return
+            end
         end
     end
 
     -- Set Owner
-    Config.Market[market]['owner'] = citizenId
-    TriggerClientEvent("brazzers-market:client:updateBooth", -1, market, 'owner', citizenId)
+    Config.Market[market]['owner'] = CID
+    TriggerClientEvent("brazzers-market:client:updateBooth", -1, market, 'owner', CID)
     TriggerClientEvent('brazzers-market:client:setVariable', src, true)
     -- Set Password
     Config.Market[market]['password'] = password
     TriggerClientEvent("brazzers-market:client:setBoothPassword", -1, market, password)
     -- Notification
-    TriggerClientEvent("DoLongHudText", src, 'You have claimed a booth!')
+    TriggerClientEvent('QBCore:Notify', src, Lang:t("primary.booth_claimed"))
 end)
 
 RegisterNetEvent('brazzers-market:server:setGroupMembers', function(market)
     local src = source
-    if not src then return end
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
+    if not market then return end
 
     local Owner = QBCore.Functions.GetPlayerByCitizenId(Config.Market[market]['owner'])
-    local citizenId = Player.PlayerData.citizenid
+    local CID = Player.PlayerData.citizenid
+    local charName = Player.PlayerData.charinfo.firstname..' '..Player.PlayerData.charinfo.lastname
 
-    if citizenId == Config.Market[market]['owner'] then return TriggerClientEvent("DoLongHudText", src, 'You\'re already part of this booth', 2) end    
+    if CID == Config.Market[market]['owner'] then return TriggerClientEvent('QBCore:Notify', src, Lang:t("error.already_part"), 'error') end    
     for marketType, _ in pairs(Config.Market) do
         for groupMember, _ in pairs(Config.Market[marketType]['groupMembers']) do
-            if Config.Market[marketType]['groupMembers'][groupMember] == citizenId then
-                TriggerClientEvent("DoLongHudText", src, 'You\'re already part of a booth!', 2)
+            if Config.Market[marketType]['groupMembers'][groupMember] == CID then
+                TriggerClientEvent('QBCore:Notify', src, Lang:t("error.already_part"), 'error')
                 return
             end
         end
     end
 
     -- Update Group Members Table
-    Config.Market[market]['groupMembers'][#Config.Market[market]['groupMembers']+1] = citizenId
+    Config.Market[market]['groupMembers'][#Config.Market[market]['groupMembers']+1] = CID
     TriggerClientEvent('brazzers-market:client:updateBooth', -1, market, 'groupMembers', json.encode(Config.Market[market]['groupMembers']))
     TriggerClientEvent('brazzers-market:client:setVariable', src, true)
     --Notification
-    TriggerClientEvent("DoLongHudText", src, 'You joined the booth!')
-    TriggerClientEvent("DoLongHudText", Owner.PlayerData.source, Player.PlayerData.charinfo.firstname..' joined the booth!')
+    TriggerClientEvent('QBCore:Notify', src, Lang:t("primary.joined_booth"))
+    TriggerClientEvent('QBCore:Notify', Owner.PlayerData.source, Lang:t("primary.global_joined_booth", { value = charName}))
 end)
 
 RegisterNetEvent('brazzers-market:server:leaveBooth', function(market)
     local src = source
-    if not src then return end
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
+    if not market then return end
 
     local Owner = QBCore.Functions.GetPlayerByCitizenId(Config.Market[market]['owner'])
-    local citizenId = Player.PlayerData.citizenid
+    local CID = Player.PlayerData.citizenid
+    local charName = Player.PlayerData.charinfo.firstname..' '..Player.PlayerData.charinfo.lastname
 
-    if Config.Market[market]['owner'] == citizenId then 
-        TriggerClientEvent("DoLongHudText", src, 'Booth Disbanded!', 2)
+    if Config.Market[market]['owner'] == CID then
+        TriggerClientEvent('QBCore:Notify', src, Lang:t("primary.disband_group"))
         resetBooth(market)
         return 
     end
-    if not next(Config.Market[market]['groupMembers']) then return TriggerClientEvent("DoLongHudText", src, 'You\'re not part of this booth!', 2) end
+    if not next(Config.Market[market]['groupMembers']) then return TriggerClientEvent('QBCore:Notify', src, Lang:t("error.not_part"), 'error') end
     for _, k in pairs(Config.Market[market]['groupMembers']) do
-        if k ~= citizenId then
-            TriggerClientEvent("DoLongHudText", src, 'You\'re not part of this booth!', 2)
+        if k ~= CID then
+            TriggerClientEvent('QBCore:Notify', src, Lang:t("error.not_part"), 'error')
             return
         end
     end
@@ -107,8 +109,8 @@ RegisterNetEvent('brazzers-market:server:leaveBooth', function(market)
     TriggerClientEvent('brazzers-market:client:updateBooth', -1, market, 'groupMembers', json.encode(Config.Market[market]['groupMembers']))
     TriggerClientEvent('brazzers-market:client:setVariable', src, false)
     -- Notification
-    TriggerClientEvent("DoLongHudText", src, 'You left the booth!')
-    TriggerClientEvent("DoLongHudText", Owner.PlayerData.source, Player.PlayerData.charinfo.firstname..' left the booth!')
+    TriggerClientEvent('QBCore:Notify', src, Lang:t("primary.left_booth"))
+    TriggerClientEvent('QBCore:Notify', Owner.PlayerData.source, Lang:t("primary.global_left_booth", { value = charName}))
 end)
 
 RegisterNetEvent('brazzers-market:server:resetMarkets', function()
@@ -125,7 +127,6 @@ end)
 
 QBCore.Functions.CreateCallback('brazzers-market:server:groupMembers', function(source, cb, market)
     local src = source
-    if not src then return end
     local Player = QBCore.Functions.GetPlayer(src)
     if not Player then return end
 

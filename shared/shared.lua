a3d670a81296be071d1f82222c94897c2b46862e
MannyOnBrazzers <mannygraphicdesign@gmail.com>
2022-11-15 02:19:45 -0500

    Added config options + set coords for booths etc

--- shared/shared.lua
+++ shared/shared.lua
@@ -1,5 +1,7 @@
 Config = Config or {}
 
+Config.Debug = false
+
 Config.Core = 'qb-core'
 Config.Target = 'qb-target'
 Config.Input = 'qb-input'
@@ -13,17 +15,21 @@ Config.StashSlots = 30
 Config.PickupWeight = 200000
 Config.PickupSlots = 25
 
-Config.AllowMultipleClaims = false -- This allows multiple booths to be claimed from the same person [RECOMMENDED: FALSE]
+Config.WipeStashOnLeave = true -- WIPES THE CONTENTS INSIDE THE STASHES OF YOUR BOOTH WHEN YOU LEAVE [RECOMMENDED: TRUE] [REQUIRES OXMYSQL]
+Config.AllowMultipleClaims = true -- THIS ALLOWS MULTIPLE BOOTHS TO BE CLAIMED FROM THE SAME PERSON [RECOMMENDED: FALSE]
 
-Config.Debug = false
+-- PolyZone Config
+Config.PierPoly = vector3(-1654.94, -1024.43, 13.02) -- JUST DON'T TOUCH
+Config.PierRadius = 150.00 -- JUST DON'T TOUCH
+
 Config.Market = {
     [1] = {
         ['booth'] = {
-            ['coords'] = vector3(-1661.88, -1018.39, 13.02),
+            ['coords'] = vector3(-1665.14, -1022.04, 13.02),
             ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-1662.48, -1022.98, 13.02),
+            ['coords'] = vector3(-1661.52, -1024.1, 12.55),
             ['heading'] = 320,
         },
         ['type'] = 'pier',
@@ -42,408 +48,278 @@ Config.Market = {
     },
     [2] = {
         ['booth'] = {
-            ['coords'] = vector3(-1670.32, -1026.5, 13.07),
+            ['coords'] = vector3(-1668.95, -1026.54, 13.02),
             ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-1666.53, -1027.87, 13.05),
+            ['coords'] = vector3(-1664.82, -1027.94, 12.67),
             ['heading'] = 320,
         },
         ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_02',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
     [3] = {
         ['booth'] = {
-            ['coords'] = vector3(-1675.12, -1032.34, 13.04),
+            ['coords'] = vector3(-1672.68, -1030.8, 13.02),
             ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-1671.29, -1033.43, 13.09),
+            ['coords'] = vector3(-1668.92, -1032.16, 12.75),
             ['heading'] = 320,
         },
         ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_03',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
     [4] = {
         ['booth'] = {
-            ['coords'] = vector3(-1678.58, -1036.31, 13.02),
-            ['heading'] = 230,
+            ['coords'] = vector3(-1681.17, -1045.21, 13.13),
+            ['heading'] = 140,
         },
         ['register'] = {
-            ['coords'] = vector3(-1674.69, -1037.52, 13.11),
-            ['heading'] = 320,
+            ['coords'] = vector3(-1677.52, -1042.26, 12.89),
+            ['heading'] = 232.01,
         },
         ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_04',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
     [5] = {
         ['booth'] = {
-            ['coords'] = vector3(-1679.18, -1041.1, 13.12),
-            ['heading'] = 140,
+            ['coords'] = vector3(-1685.37, -1046.22, 13.02),
+            ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-1678.86, -1042.64, 13.13),
+            ['coords'] = vector3(-1681.46, -1048.49, 12.74),
             ['heading'] = 320,
         },
         ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_05',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
     [6] = {
         ['booth'] = {
-            ['coords'] = vector3(-1686.34, -1050.09, 13.16),
-            ['heading'] = 140,
+            ['coords'] = vector3(-1692.91, -1055.19, 13.02),
+            ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-1685.01, -1049.77, 13.14),
+            ['coords'] = vector3(-1689.07, -1057.23, 12.92),
             ['heading'] = 320,
         },
         ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_06',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
     [7] = {
         ['booth'] = {
-            ['coords'] = vector3(-1690.85, -1050.62, 13.02),
-            ['heading'] = 50,
+            ['coords'] = vector3(-1696.79, -1059.88, 13.02),
+            ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-1686.62, -1051.91, 13.15),
+            ['coords'] = vector3(-1692.53, -1061.44, 12.85),
             ['heading'] = 320,
         },
         ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_07',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
     [8] = {
         ['booth'] = {
-            ['coords'] = vector3(-1694.52, -1055.21, 13.02),
-            ['heading'] = 50,
+            ['coords'] = vector3(-1696.83, -1064.03, 13.11),
+            ['heading'] = 320,
         },
         ['register'] = {
-            ['coords'] = vector3(-1690.35, -1056.2, 13.09),
-            ['heading'] = 320,
+            ['coords'] = vector3(-1698.47, -1068.68, 13.15),
+            ['heading'] = 51,
         },
         ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_08',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
     [9] = {
         ['booth'] = {
-            ['coords'] = vector3(-1700.84, -1066.88, 13.14),
-            ['heading'] = 320,
+            ['coords'] = vector3(-1709.35, -1078.42, 13.08),
+            ['heading'] = 140,
         },
         ['register'] = {
-            ['coords'] = vector3(-1698.37, -1065.79, 13.13),
-            ['heading'] = 320,
-        },
-        ['type'] = 'pier',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [10] = {
-        ['booth'] = {
-            ['coords'] = vector3(-1705.17, -1067.86, 13.02),
-            ['heading'] = 230,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-1701.67, -1068.65, 13.11),
-            ['heading'] = 320,
-        },
-        ['type'] = 'pier',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [11] = {
-        ['booth'] = {
-            ['coords'] = vector3(-1708.61, -1072.01, 13.02),
-            ['heading'] = 230,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-1706.5, -1074.63, 13.11),
-            ['heading'] = 320,
-        },
-        ['type'] = 'pier',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [12] = {
-        ['booth'] = {
-            ['coords'] = vector3(-1722.44, -1088.56, 13.02),
-            ['heading'] = 230,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-1718.46, -1089.17, 13.03),
-            ['heading'] = 320,
-        },
-        ['type'] = 'pier',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [13] = {
-        ['booth'] = {
-            ['coords'] = vector3(-1726.12, -1093.06, 13.02),
-            ['heading'] = 230,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-1722.04, -1093.66, 13.05),
-            ['heading'] = 320,
-        },
-        ['type'] = 'pier',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [14] = {
-        ['booth'] = {
-            ['coords'] = vector3(-1729.55, -1097.52, 13.02),
-            ['heading'] = 50,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-1727.46, -1097.93, 13.09),
-            ['heading'] = 320,
-        },
-        ['type'] = 'pier',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [15] = {
-        ['booth'] = {
-            ['coords'] = vector3(-1733.31, -1101.79, 13.02),
-            ['heading'] = 50,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-1729.71, -1103.19, 13.14),
+            ['coords'] = vector3(-1705.6, -1076.3, 13.01),
             ['heading'] = 140,
         },
         ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_09',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
-    [16] = {
+    [10] = {
         ['booth'] = {
-            ['coords'] = vector3(-271.18, 6130.0, 31.5),
-            ['heading'] = 45,
+            ['coords'] = vector3(-1713.42, -1079.57, 13.02),
+            ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-273.87, 6132.11, 31.51),
-            ['heading'] = 315,
+            ['coords'] = vector3(-1708.97, -1081.46, 12.96),
+            ['heading'] = 320,
         },
-        ['type'] = 'paleto',
+        ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_10',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
-    [17] = {
+    [11] = {
         ['booth'] = {
-            ['coords'] = vector3(-274.87, 6126.24, 31.52),
-            ['heading'] = 45,
+            ['coords'] = vector3(-1720.61, -1088.27, 13.02),
+            ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-277.42, 6125.29, 31.51),
-            ['heading'] = 225,
+            ['coords'] = vector3(-1716.31, -1089.71, 12.83),
+            ['heading'] = 320,
         },
-        ['type'] = 'paleto',
+        ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_11',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
-    [18] = {
+    [12] = {
         ['booth'] = {
-            ['coords'] = vector3(-286.51, 6111.99, 31.44),
-            ['heading'] = 45,
+            ['coords'] = vector3(-1724.29, -1092.86, 13.02),
+            ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-288.59, 6116.21, 31.61),
-            ['heading'] = 315,
+            ['coords'] = vector3(-1721.7, -1096.66, 12.83),
+            ['heading'] = 320,
         },
-        ['type'] = 'paleto',
+        ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_12',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
+        },
     },
-    [19] = {
+    [13] = {
         ['booth'] = {
-            ['coords'] = vector3(-290.47, 6108.16, 31.41),
-            ['heading'] = 45,
+            ['coords'] = vector3(-1728.03, -1097.36, 13.02),
+            ['heading'] = 230,
         },
         ['register'] = {
-            ['coords'] = vector3(-293.81, 6109.2, 31.53),
-            ['heading'] = 315,
+            ['coords'] = vector3(-1723.92, -1099.29, 12.77),
+            ['heading'] = 320,
         },
-        ['type'] = 'paleto',
+        ['type'] = 'pier',
         ['owner'] = nil,
         ['groupMembers'] = {},
         ['password'] = nil,
-    },
-    [20] = {
-        ['booth'] = {
-            ['coords'] = vector3(-294.12, 6104.0, 31.39),
-            ['heading'] = 45,
+        -- DUI SETTINGS --
+        ['boothDUI'] = {
+            ['ytd'] = 'fm_tent_scr_textures',
+            ['ytdname'] = 'fm_tent_screen_13',
+            ['width'] = 1024,
+            ['height'] = 1024,
+            ['url'] = Config.DefaultImage,
+            ['dui'] = nil,
         },
-        ['register'] = {
-            ['coords'] = vector3(-296.56, 6108.02, 31.53),
-            ['heading'] = 315,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [21] = {
-        ['booth'] = {
-            ['coords'] = vector3(-312.94, 6105.6, 31.5),
-            ['heading'] = 135,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-311.45, 6108.28, 31.5),
-            ['heading'] = 45,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [22] = {
-        ['booth'] = {
-            ['coords'] = vector3(-316.73, 6109.43, 31.5),
-            ['heading'] = 135,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-315.09, 6111.59, 31.6),
-            ['heading'] = 45,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [23] = {
-        ['booth'] = {
-            ['coords'] = vector3(-314.27, 6122.59, 31.59),
-            ['heading'] = 45,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-311.45, 6121.87, 31.52),
-            ['heading'] = 315,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [24] = {
-        ['booth'] = {
-            ['coords'] = vector3(-309.37, 6127.48, 31.5),
-            ['heading'] = 45,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-307.64, 6123.79, 31.5),
-            ['heading'] = 315,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [25] = {
-        ['booth'] = {
-            ['coords'] = vector3(-305.6, 6131.33, 31.55),
-            ['heading'] = 45,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-302.82, 6128.61, 31.5),
-            ['heading'] = 315,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [26] = {
-        ['booth'] = {
-            ['coords'] = vector3(-297.72, 6135.9, 31.48),
-            ['heading'] = 40,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-295.01, 6134.39, 31.46),
-            ['heading'] = 135,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [27] = {
-        ['booth'] = {
-            ['coords'] = vector3(-300.93, 6117.53, 31.5),
-            ['heading'] = 315,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-297.97, 6121.1, 31.5),
-            ['heading'] = 225,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [28] = {
-        ['booth'] = {
-            ['coords'] = vector3(-301.54, 6117.21, 31.5),
-            ['heading'] = 315,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-300.73, 6114.56, 31.5),
-            ['heading'] = 135,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [29] = {
-        ['booth'] = {
-            ['coords'] = vector3(-299.45, 6099.46, 31.43),
-            ['heading'] = 225,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-303.04, 6101.36, 31.5),
-            ['heading'] = 315,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
-    },
-    [30] = {
-        ['booth'] = {
-            ['coords'] = vector3(-303.28, 6095.62, 31.39),
-            ['heading'] = 225,
-        },
-        ['register'] = {
-            ['coords'] = vector3(-306.57, 6099.56, 31.5),
-            ['heading'] = 315,
-        },
-        ['type'] = 'paleto',
-        ['owner'] = nil,
-        ['groupMembers'] = {},
-        ['password'] = nil,
     },
 }
